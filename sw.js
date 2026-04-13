dd25600a4d449789f3860b98230f9d16a93482da
Honorwhite <1onurakbas@gmail.com>
2026-04-13 07:22:51 +0300

    gg

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/assets/css/style.css',
+        '/assets/css/style.css?v=1.0.1',
         '/assets/js/main.js'
       ]);
     })
